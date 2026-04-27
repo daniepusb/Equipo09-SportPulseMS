b113feabe0901ecae6c7a0e1de27ac8c7fc9de59
dio-quincar <dio-quincar@outlook.com>
2026-04-27 12:37:59 -0500

    criteria complete HU-10/HU-11

--- ms-leagues/src/main/java/com/sportpulse/ms_leagues/service/LeagueService.java
+++ ms-leagues/src/main/java/com/sportpulse/ms_leagues/service/LeagueService.java
@@ -3,6 +3,7 @@ package com.sportpulse.ms_leagues.service;
 import java.util.List;
 import com.sportpulse.ms_leagues.model.League;
 import com.sportpulse.ms_leagues.repository.LeagueRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,10 +15,12 @@ public class LeagueService {
         this.leagueRepository = leagueRepository;
     }
 
+    @Cacheable(value = "leagues", key = "#country + ':' + #season")
     public List<League> getAllLeagues(String country, Integer season) {
         return leagueRepository.findAll(country, season);
     }
 
+    @Cacheable(value = "leagues", key = "#leagueId")
     public League getLeagueById(long leagueId) {
         return leagueRepository.findById(leagueId);
     }
