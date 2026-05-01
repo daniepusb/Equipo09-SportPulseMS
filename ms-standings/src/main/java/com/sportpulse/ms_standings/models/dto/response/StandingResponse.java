661f78ee906ec9aba2ee07c497fcbc85607c181e
dio-quincar <dio-quincar@outlook.com>
2026-05-01 11:46:52 -0500

    Add Seriazable for Redis cache in teams and standings ms

--- ms-standings/src/main/java/com/sportpulse/ms_standings/models/dto/response/StandingResponse.java
+++ ms-standings/src/main/java/com/sportpulse/ms_standings/models/dto/response/StandingResponse.java
@@ -1,15 +1,16 @@
 package com.sportpulse.ms_standings.models.dto.response;
 
+import java.io.Serializable;
 import java.util.List;
 
 public record StandingResponse(
         LeagueInfo league,
         List<TeamStanding> standings
-) {
-    public record LeagueInfo(Long id, String name, String country, Integer season) {}
+) implements Serializable {
+    public record LeagueInfo(Long id, String name, String country, Integer season) implements Serializable {}
     public record TeamStanding(Integer rank, TeamInfo team, Integer points,
                                Integer played, Integer won, Integer drawn,
                                Integer lost, Integer goalsFor, Integer goalsAgainst,
-                               Integer goalDifference, String form) {}
-    public record TeamInfo(Long id, String name, String logo) {}
+                               Integer goalDifference, String form) implements Serializable {}
+    public record TeamInfo(Long id, String name, String logo) implements Serializable {}
 }
--- ms-teams/src/main/java/com/sportpulse/ms_teams/model/Stadium.java
+++ ms-teams/src/main/java/com/sportpulse/ms_teams/model/Stadium.java
@@ -1,9 +1,11 @@
 package com.sportpulse.ms_teams.model;
 
+import java.io.Serializable;
+
 public record Stadium(
     String name,
     String address,
     String city,
     Integer capacity,
     String surface
-) {}
+) implements Serializable {}
